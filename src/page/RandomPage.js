9ae7237e1f13c5cb4e66cbdb9bd407eef56e8d0d
Edwin RA <edwinra240@gmail.com>
2022-11-07 21:56:15 -0600

    "Nice"

--- src/page/RandomPage.js
+++ src/page/RandomPage.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from "react";
+
+const URL = "www.thecocktaildb.com/api/json/v1/1/random.php";
 
 function RandomPage() {
+  const [RandCocktail, setRandCocktail] = React.useState({});
 
-    return (
-      <>
-      <h1>Hi</h1>
-      </>
-    );
-  }
-  
-  export default RandomPage;
+  React.useEffect(() => {
+    console.log("en el use effect");
+    getCocktail();
+  }, []);
+
+  const getCocktail = async () => {
+    const response = await fetch(URL);
+    const cocktail = await response.json();
+    console.log(cocktail);
+    setRandCocktail(cocktail);
+  };
+
+  return (
+    <>
+      <h1>{RandCocktail.strDrink}</h1>
+    </>
+  );
+}
+
+export default RandomPage;
